af1b33738851474b8864c161306e34a7fbe1f49e
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-13 12:33:27 +0500

    Add files via upload
    Personal message

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+//Personal Message: Store a person name in a variable, and print a message to that person. Your message should be simple, such as, 
+//"Hello Eric, would you like to learn some python today? "
+let personName = "Eric";
+console.log(`Hellow ${personName}, would you like to learn some python today?`);
---
+++ app.ts
@@ -0,0 +1,9 @@
+#!/user/bin/env node
+
+
+//Personal Message: Store a person name in a variable, and print a message to that person. Your message should be simple, such as, 
+//"Hello Eric, would you like to learn some python today? "
+
+
+let personName: String= "Eric";
+console.log (`Hellow ${personName}, would you like to learn some python today?`);
